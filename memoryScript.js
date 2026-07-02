593f3f73b8d6758ef1493fd59c4aebde265653d4
JordanDemaret <jordandemaret12@gmail.com>
2026-07-02 15:04:01 +0200

    Add initial game files including HTML, CSS, JS and images.

---
+++ memoryScript.js
@@ -0,0 +1,82 @@
+const game_board = document.getElementById("game-board")
+const pNrbAttempt = document.getElementById("nbrAttempt")
+//
+
+const data = ["A","B","C","D","E","F","G","H", "A","B","C","D","E","F","G","H"]
+
+
+//data.sort( () => Math.random()-0.5)
+var i = data.length, j, temp;
+while(--i > 0){
+    j = Math.floor(Math.random()*(i+1));
+    temp = data[j];
+    data[j] = data[i];
+    data[i] = temp;
+}
+
+let nbrPair=0
+let firstCard = 0
+let canEdit = true
+let nbrAttempt=0
+
+
+pNrbAttempt.textContent =`number of attempt ${nbrAttempt}`
+
+data.forEach(function(elem, index){
+
+    const new_card = document.createElement('img')
+    new_card.src= 'Dos.png';
+    new_card.alt= elem
+    new_card.className = "flipped"
+
+    new_card.addEventListener('click', () =>{    
+        console.log(canEdit)
+        if (!canEdit)
+            return
+    
+        new_card.src=elem+".png"
+        new_card.className=""
+        if (firstCard === 0)
+            firstCard=new_card
+        else{
+            compare(firstCard, new_card)
+        }
+    })
+
+    game_board.append(new_card)
+})
+
+
+
+function compare(elem1, elem2){
+    nbrAttempt++
+    pNrbAttempt.textContent =`number of attempt ${nbrAttempt}`
+    firstCard=0
+    if (elem1.alt == elem2.alt){
+        elem1.disable=true
+        elem2.disable=true
+        nbrPair++
+        isEnd()
+    }
+    else{
+        canEdit = false
+        setTimeout( (elem1, elem2) => {
+            elem1.src = "Dos.png"
+            elem1.className="flipped"
+            elem2.src = "Dos.png"
+            elem2.className="flipped"
+            canEdit=true
+        }, 500,elem1,elem2)
+        
+        
+    }
+}
+
+function isEnd(){
+    if (nbrPair === 8){
+        const h2 = document.createElement('h2')
+        h2.textContent = 'The Game is End. You are a Winer'
+
+        game_board .before(h2)
+    }
+}
